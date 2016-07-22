671a286d1332c9314848c3fb3a54aaecd0a0c83d
Marimuthu <marimuthu.m@ideas2it.com>
2016-07-22 11:41:21 +0530

    FEATURE:added three pojo classes

---
+++ src/model/UserRole.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package model;
+
+public class UserRole {
+	private int userId;
+	private int roleId;
+        
+    public UserRole(int userId, int roleId) {
+    	this.userId = userId;
+    	this.roleId = roleId;
+    }
+    
+    public void setUserId(int userId) {
+    	this.userId = userId;
+    }
+    public void setRoleId(int roleId) {
+    	this.roleId = roleId;
+    }
+    
+    public int getUserId() {
+    	return userId;
+    }
+    public int getRoleId() {
+    	return roleId;
+    }
+}
